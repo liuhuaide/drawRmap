beabe35a0eba09194347606f24303d1c363ceb6d
liuhuaide <jcakcino@gmail.com>
2024-10-13 22:06:44 +0200

    update

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# drawRmap
+# Shiny Analysis App
+
+Welcome to the Shiny Analysis App, a Shiny application based on R designed for interactive data analysis using the CntoAPI package.
+
+## Introduction
+
+This application allows users to select different datasets (such as `mtcars` or `iris`) and use a slider to choose the number of observations to display. The application dynamically showcases a table view of the selected dataset.
+
+## Features
+
+- Dataset selection
+- Slider for selecting the number of observations to display
+- Dynamic table updates
+
+## Tech Stack
+
+- R
+- Shiny
+- CntoAPI
+
+## Prerequisites
+
+Before running the application, make sure you have the following R packages installed:
+
+- `shiny`
+- `CntoAPI`
+
+You can install these packages using the following commands:
+
+```r
+install.packages("shiny")
+# If you haven't installed CntoAPI yet, install it from GitHub
+if (!requireNamespace("remotes", quietly = TRUE)) {
+    install.packages("remotes")
+}
+remotes::install_github("liuhuaide/CntoAPI")
---
+++ app.R
@@ -0,0 +1,51 @@
+
+library(shiny)
+library(shiny.fluent)
+
+if (!requireNamespace("CntoAPI", quietly = TRUE)) {
+    if (!requireNamespace("remotes", quietly = TRUE)) {
+        install.packages("remotes")
+    }
+    remotes::install_github("liuhuaide/CntoAPI")
+}
+
+library(CntoAPI)
+
+# Define UI
+ui <- fluidPage(
+    titlePanel("Interactive Map Viewer"),
+    
+    sidebarLayout(
+        sidebarPanel(
+            textInput("location", "Enter a Location:", value = "Linköping"),
+            selectInput("maptype", "Choose Map Type:", 
+                        choices = c("outdoors","stamen_toner", "stamen_toner_lite", 
+                                    "stamen_terrain", "stamen_terrain_background",
+                                    "stamen_terrain_labels", "stamen_terrain_lines",
+                                    "alidade_smooth", "alidade_smooth_dark")),
+            sliderInput("zoom",
+                        "Select Zoom Level:", 
+                        min = 5, max = 18, value = 11)
+        ),
+        
+        mainPanel(
+            plotOutput("mapPlot")
+        )
+    )
+)
+
+# Define server
+server <- function(input, output) {
+    
+    reactive_location <- reactive({
+        req(input$location) 
+        input$location
+    })
+    
+    output$mapPlot <- renderPlot({
+        drawRmap(location = reactive_location(),
+                 maptype = input$maptype, zoom = input$zoom)
+    })
+}
+
+shinyApp(ui = ui, server = server)
